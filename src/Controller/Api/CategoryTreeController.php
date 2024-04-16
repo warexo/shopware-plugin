70f6f56756f32b38a7696097e387323ebc0bc80d
alexanderkludt <coding@aggrosoft.de>
2024-04-16 14:52:07 +0200

    feat: Shopware 6.6 compat

--- src/Controller/Api/CategoryTreeController.php
+++ src/Controller/Api/CategoryTreeController.php
@@ -3,16 +3,14 @@
 namespace Warexo\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Shopware\Core\Framework\Context;
+use Symfony\Component\Routing\Attribute\Route;
 use Warexo\Service\CategoryTreeLoader;
 
-/**
- * @Route(defaults={"_routeScope"={"api"}})
- */
+#[Route(defaults: ['_routeScope' => ['storefront']])]
 class CategoryTreeController extends AbstractController
 {
 
@@ -23,9 +21,7 @@ class CategoryTreeController extends AbstractController
         $this->categoryTreeLoader = $categoryTreeLoader;
     }
 
-    /**
-     * @Route("/api/_action/warexo-category-list", name="api.action.warexo.category.list", methods={"GET"})
-     */
+    #[Route(path: '/api/_action/warexo-category-list', name: 'api.action.warexo.category.list', methods: ['GET'])]
     public function getCategoryListAction(Request $request, Context $context): JsonResponse
     {
         return new JsonResponse($this->categoryTreeLoader->loadFlat($context));
@@ -33,8 +29,7 @@ class CategoryTreeController extends AbstractController
 
     //@TODO: add tree capabilities to warexo to display this
     /**
-     * @Route("/api/_action/warexo-category-tree", name="api.action.warexo.category-tree", methods={"GET"})
-
+    #[Route(path: '/api/_action/warexo-category-tree', name: 'api.action.warexo.category-tree', methods: ['GET'])]
     public function getCategoryTreeAction(Request $request, Context $context): JsonResponse
     {
         return new JsonResponse($this->categoryTreeLoader->load($context));
