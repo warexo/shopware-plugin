e1ab0ba5e6def2a806542672fcbcb2b4999686ca
alexanderkludt <coding@aggrosoft.de>
2024-06-26 10:53:16 +0200

    feat: show warexo info on property group

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,12 +1,13 @@
 import './sw-desktop-override'
 import './sw-page-override'
 import './sw-cms-detail-override'
+import './sw-property-detail-base-override'
 
 import './sw-cms/blocks/commerce/product-options'
 import './sw-cms/elements/product-options'
 
-import deDE from './sw-cms/snippet/de-DE.json';
-import enGB from './sw-cms/snippet/en-GB.json';
+import deDE from './snippet/de-DE.json';
+import enGB from './snippet/en-GB.json';
 
 Shopware.Locale.extend('de-DE', deDE);
 Shopware.Locale.extend('en-GB', enGB);
---
+++ src/Resources/app/administration/src/sw-property-detail-base-override/index.js
@@ -0,0 +1,5 @@
+import template from './sw-property-detail-base.html.twig';
+
+Shopware.Component.override('sw-property-detail-base', {
+    template
+})
