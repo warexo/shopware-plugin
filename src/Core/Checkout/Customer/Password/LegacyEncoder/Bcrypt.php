e6ebdb0465d8f9dbd00fbdcf866b0808d73febda
alexanderkludt <coding@aggrosoft.de>
2026-04-15 15:36:33 +0200

    feat: add bcrypt legacy encoder

---
+++ src/Core/Checkout/Customer/Password/LegacyEncoder/Bcrypt.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Warexo\Core\Checkout\Customer\Password\LegacyEncoder;
+
+use Shopware\Core\Checkout\Customer\Password\LegacyEncoderInterface;
+use Shopware\Core\Framework\Log\Package;
+
+#[Package('checkout')]
+class Bcrypt implements LegacyEncoderInterface
+{
+    public function getName(): string
+    {
+        return 'Bcrypt';
+    }
+
+    public function isPasswordValid(#[\SensitiveParameter] string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
+}
